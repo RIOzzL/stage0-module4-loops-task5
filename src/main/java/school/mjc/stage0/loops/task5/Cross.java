56c7d76c69fc97625fc3d8e4e7913be6994326db
Roman Gulevich <R.I.Gulevich@gmail.com>
2022-07-02 15:34:11 +0300

    "Module4 -Task 5 Hourglass - done + changed pom.xml"

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,17 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
-
+        for (int i = 1; i <= sideLength; i++) {
+            for (int j = 1; j <= sideLength; j++) {
+                if (i == (int) (sideLength / 2.0 + 0.5)) {
+                    System.out.print("8");
+                } else if (j == (int) (sideLength / 2.0 + 0.5) & i != (sideLength / 2.0 + 0.5)) {
+                    System.out.print("8");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
+        for(int i=0;i<height
+                ;i++){
+            if(i<height/2){
+                for (int j=1;j<=i;j++) System.out.print(" ");
+                for (int j=1;j<=height-2*i;j++) System.out.print("8");
+            }else{
+                for (int j=1;j<=height-i-1;j++) System.out.print(" ");
+                for (int j=1;j<=2*(i-height/2)+1;j++) System.out.print("8");
+            }
+            System.out.println();
+        }
     }
 }
